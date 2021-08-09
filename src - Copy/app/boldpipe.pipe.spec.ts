b609245868d6821816652d72d0fa8ced357466ef
krishnamohanyedla <krishnamohanyedla@gmail.com>
2021-08-09 22:03:02 +0530

    updatedui

--- src - Copy/app/boldpipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BoldpipePipe } from './boldpipe.pipe';
-
-describe('BoldpipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new BoldpipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
